178bbe8de9ce74e14a0b434f6cb7bb3516921a84
flavien-hugs <geekactux@gmail.com>
2023-01-17 12:19:01 +0000

    udpate routes

--- core/views.py
+++ core/views.py
@@ -1,18 +1,25 @@
 from flask import Blueprint, render_template
- 
+
+from .models import Post
+
 post = Blueprint("post", __name__, url_prefix='/')
 
 
 @post.route("/")
 def home_page():
+    
+    posts = Post.query.all()[:4]
+
     return render_template(
         "index.html",
+        posts_all=posts,
         page_title="Page d'accueil"
     )
 
 
 @post.route("/contact/")
 def contact_page():
+
 	return render_template(
         "pages/contact.html",
         page_title="Contact page"
