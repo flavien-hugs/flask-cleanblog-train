aff7bfaf2054fa7aedbd47720fee25ee0c9f1288
flavien-hugs <geekactux@gmail.com>
2023-01-19 13:27:10 +0000

    edit post list and detail page

--- core/views.py
+++ core/views.py
@@ -4,25 +4,47 @@ from .models import Post
 
 post = Blueprint("post", __name__, url_prefix='/')
 
+posts = Post.query.all()
 
 @post.route("/")
 def home_page():
-    
-    posts = Post.query.all()[:4]
-
     return render_template(
         "index.html",
+        posts_all=posts[:4],
+        page_title="Accueil",
+        subheading="Bienvenu(e) sur Clean Blog"
+    )
+
+
+@post.route("/blog")
+def blog_page():
+    return render_template(
+        "pages/blog/list.html",
         posts_all=posts,
-        page_title="Page d'accueil"
+        page_title="Blog",
+        subheading="Liste des articles"
+    )
+
+
+@post.route("/blog/<int:post_id>")
+def blog_detail_page(post_id):
+
+    article = Post.query.get_or_404(post_id)
+    
+    return render_template(
+        "pages/blog/detail.html",
+        post=article,
+        page_title=article.post_title,
+        subheading=article.post_subtitle
     )
 
 
 @post.route("/contact/")
 def contact_page():
-
 	return render_template(
         "pages/contact.html",
-        page_title="Contact page"
+        page_title="Contact",
+        subheading="Laissez-nous un message"
     )
 
 
@@ -30,5 +52,6 @@ def contact_page():
 def support_page():
     return render_template(
         "pages/support.html",
-        page_title="Support page"
+        page_title="Support",
+        subheading="Besoin d'aides ?"
     )
